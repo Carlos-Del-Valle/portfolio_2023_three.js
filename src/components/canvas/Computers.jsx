fab1e8017a33ba3ce04de68355ec344586724b60
Carlos Del Valle <cadelvalledieguez@gmail.com>
2023-10-10 18:43:44 +1100

    Add GLtf Loader Hero section + Bug Fixing

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,13 +1,33 @@
-import { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  // const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+  
 
-  return <mesh></mesh>;
+  return (
+  <mesh>
+    <hemisphereLight intensity={0.15} groundColor="black"/>
+    <pointLight intensity={1}/>
+    <primitive object={computer.scene}/>
+    
+  </mesh>)
 };
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+    frameloop="demand"
+    shadows
+    camera={{ position:[20,3,5], fov:25}}
+    gl={{preserveDrawingBuffer: true}}
+    >
+
+    </Canvas>
+  )
+}
 
 export default Computers;
